f14908c985ca95bf8618904f88929347e0c108f9
Habib Ahmed <habib.ahmed@bellmedia.ca>
2021-10-30 10:17:29 -0400

    Revert "added storage account"
    This reverts commit 8409260b556af42578927adf0658e25ba69b1414.

--- terraformscripts/main.tf
+++ terraformscripts/main.tf
@@ -68,15 +68,3 @@ resource "azurerm_windows_virtual_machine" "example" {
     version   = "latest"
   }
 }
-
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
