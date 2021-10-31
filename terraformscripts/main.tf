979319b30a04223a715ff1709ef58c535eccb96e
Habib Ahmed <habib.ahmed@bellmedia.ca>
2021-10-31 15:39:10 -0400

    added storage account in the class

--- terraformscripts/main.tf
+++ terraformscripts/main.tf
@@ -101,3 +101,15 @@ resource "azurerm_app_service" "example" {
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
